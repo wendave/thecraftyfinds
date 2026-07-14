8acc59a742a0fbc7c7675c3e41dbabafa6e9a398
petschoice-owen <owen.caisip@petschoice.co.uk>
2026-07-14 23:19:52 +0800

    dynamic background-colors depending on content; editable table headings

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(function () {
-
     function formatMoney(value) {
         return value.toLocaleString("en-PH", {
             minimumFractionDigits: 2,
@@ -8,19 +7,13 @@ $(function () {
     }
 
     function computeGroup(group) {
-
         const column = $('.table-column-data[data-group="' + group + '"]');
-
         const qty = parseInt(column.find(".group-qty").val()) || 0;
-
         let subtotal = 0;
 
         column.find(".item-checkbox:checked").each(function () {
-
             const price = parseFloat($(this).closest(".item").data("price"));
-
             subtotal += price * qty;
-
         });
 
         const savings = subtotal * 0.10;
@@ -28,7 +21,6 @@ $(function () {
 
         $("#" + group + "-total").val(formatMoney(subtotal));
         $("#" + group + "-discount").val(formatMoney(total));
-
         $("#subtotal_" + group).text(formatMoney(total));
 
         return {
@@ -36,43 +28,27 @@ $(function () {
             savings: savings,
             total: total
         };
-
     }
 
     function computeEverything() {
-
         const principal = computeGroup("principal");
         const bridesmaid = computeGroup("bridesmaid");
         const groomsmen = computeGroup("groomsmen");
 
-        const grandTotal =
-            principal.total +
-            bridesmaid.total +
-            groomsmen.total;
-
-        const totalSavings =
-            principal.savings +
-            bridesmaid.savings +
-            groomsmen.savings;
+        const grandTotal = principal.total + bridesmaid.total + groomsmen.total;
+        const totalSavings = principal.savings + bridesmaid.savings + groomsmen.savings;
 
         const deposit = parseFloat($("#total_deposit").val()) || 0;
-
         const balance = Math.max(0, grandTotal - deposit);
 
         $("#total_grand").text(formatMoney(grandTotal));
         $("#total_savings").text(formatMoney(totalSavings));
         $("#total_balance").text(formatMoney(balance));
-
     }
 
-    // Checkbox changes
-    $(document).on("change", ".item-checkbox", computeEverything);
-
-    // Quantity changes
-    $(document).on("input", ".group-qty", computeEverything);
-
-    // Deposit changes
-    $("#total_deposit").on("input", computeEverything);
+    $(document).on("change", ".item-checkbox", computeEverything); // Checkbox changes
+    $(document).on("input", ".group-qty", computeEverything); // Quantity changes
+    $("#total_deposit").on("input", computeEverything); // Deposit changes
 
     computeEverything();
 
@@ -124,4 +100,17 @@ $(function () {
     }
     clearInputs();
 
+
+    function setBackgroundColor() {
+        var oddEvenChecker = $('.table-info .table-column-placeholder .item').length;
+        if (oddEvenChecker % 2 !== 0) { // is odd
+            $('.table-info .quantity').addClass('mobile-even');
+            $('.table-info .summary').addClass('desktop-start-even mobile-start-odd');
+        } 
+        else { // is even
+            $('.table-info .quantity').addClass('mobile-odd');
+            $('.table-info .summary').addClass('desktop-start-odd mobile-start-even');
+        }
+    }
+    setBackgroundColor();
 });
